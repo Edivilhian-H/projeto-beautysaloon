c63656e22d42521b9d067dcbbebb761b9628700e
Edivilhian-H <edivilhian1k@gmail.com>
2022-05-29 14:08:04 -0300

    atualizei Projeto

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+   #about .image, #about .text,
+   #services header, #services .cards,
+   #testimonials header, #testimonials .testimonials,
+   #contact .text, #contact .links`,
+  { interval: 100 }
+)
