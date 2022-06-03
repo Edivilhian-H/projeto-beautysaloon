9250472f4ff79c84e91be0c7cf53cfef21396e97
Edivilhian-H <edivilhian1k@gmail.com>
2022-06-03 12:11:29 -0300

    Projeto completo!

--- script.js
+++ script.js
@@ -31,7 +31,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
@@ -51,10 +57,35 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenu() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 window.addEventListener('scroll', function () {
   if (window.scrollY >= 560) {
     upBt.classList.add('show')
   } else {
     upBt.classList.remove('show')
   }
+  activateMenu()
 })
